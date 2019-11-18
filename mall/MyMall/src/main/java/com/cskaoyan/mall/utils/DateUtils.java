7512980d0216e192b331daa674404d4a6c0cbe10
ITXiaoShiTou <957923132@qq.com>
2019-11-18 19:26:57 +0800

    config模块 修改

--- mall/MyMall/src/main/java/com/cskaoyan/mall/utils/DateUtils.java
+++ mall/MyMall/src/main/java/com/cskaoyan/mall/utils/DateUtils.java
@@ -2,15 +2,16 @@ package com.cskaoyan.mall.utils;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
-
 /**
- * Created by IntelliJ IDEA
- *
- * @author zhanghj
- * @date 2019/11/16
- * @time 15:52
+ * 时间工具类
  */
+
 public class DateUtils {
+
+    /**
+     * 获取当前时间
+     * @return 返回一个2019-04-16 09:08:30格式的当前时间 类型为String
+     */
     public static String currentDateToString(){
         Date date = new Date();
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH-mm-ss");
