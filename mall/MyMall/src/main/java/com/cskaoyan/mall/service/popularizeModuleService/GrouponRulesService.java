f04719bc4cf9349a52117e3a0a4f1a70dbfdc332
HancongZhao <3314792296@qq.com>
2019-11-18 23:38:35 +0800

    还差团购活动以及创建接口 需要调用商品查询接口和图片上传接口 还差两个创建接口

--- mall/MyMall/src/main/java/com/cskaoyan/mall/service/popularizeModuleService/GrouponRulesService.java
+++ mall/MyMall/src/main/java/com/cskaoyan/mall/service/popularizeModuleService/GrouponRulesService.java
@@ -2,9 +2,11 @@ package com.cskaoyan.mall.service.popularizeModuleService;
 
 import com.cskaoyan.mall.bean.generator.Goods;
 import com.cskaoyan.mall.bean.generator.popularizeModule.GrouponRules;
+import com.cskaoyan.mall.bean.generator.popularizeModule.ListRecord;
 import com.cskaoyan.mall.bean.jsonbean.popularizeModuleJsonBean.GrouponRulesJson;
 
 import java.util.List;
+import java.util.Map;
 
 public interface GrouponRulesService {
     List<GrouponRules> getGrouponRules(Integer page, Integer limit, Integer goodsId, String sort, String order);
@@ -16,4 +18,6 @@ public interface GrouponRulesService {
     Integer queryGoodsId(Integer goodsId);
 
     GrouponRules createGrouponRule(GrouponRulesJson grouponRulesJson);
+
+    List<Map<String,Object>> queryListRecords(Integer page, Integer limit, String sort, String order, Integer goodsId);
 }
