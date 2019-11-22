f7cded587d809ab9edc3e0bd34636684444e28ae
residentOfTouHou <zhanghanjie19@163.com>
2019-11-22 11:45:29 +0800

    s

--- mall/MyMall/src/main/java/com/cskaoyan/mall/service/impl/CollectServiceImpl.java
+++ mall/MyMall/src/main/java/com/cskaoyan/mall/service/impl/CollectServiceImpl.java
@@ -49,7 +49,10 @@ public class CollectServiceImpl implements CollectService {
     public Map<String, Object> findCollectByCondition(PageSplit pageSplit) {
         Integer page = pageSplit.getPage();
         Integer size = pageSplit.getSize();
-        Integer limit = size;
+        Integer limit = pageSplit.getLimit();
+        if(size!=null){
+            limit = size;
+        }
         PageHelper.startPage(page, limit);
         Integer valueId = pageSplit.getValueId();
         Integer userId = pageSplit.getUserId();
